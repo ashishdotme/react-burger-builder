f8b5cc6d3cd66ce63181ec1680006c154c5a08bd
Ashish Patel <ashishsushilpatel@gmail.com>
2019-02-10 22:07:38 +0530

    Pass ingredients to the burger component from the builder

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+import "./Burger.css";
 
-export default class Burger extends Component {
-  render() {
-    return (
-      <div>
-        Ashish
-      </div>
-    )
-  }
+interface BurgerProps {
+  ingredients: Record<string, number>;
 }
+const Burger: React.SFC<BurgerProps> = props => {
+  const transformedIngredients = Object.keys(props.ingredients).map(
+    (igKey: string) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    }
+  );
+  return (
+    <div className="Burger">
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default Burger;
--- src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import Burger from "../../components/Burger/Burger";
 
-export default class BurgerBuilder extends Component {
+interface BurgerBuilderState {
+  ingredients: Record<string, number>;
+}
+export default class BurgerBuilder extends Component<any, BurgerBuilderState> {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 1,
+      meat: 2
+    }
+  };
   render() {
     return (
       <React.Fragment>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </React.Fragment>
     );
